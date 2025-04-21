3d571b0ef8d3a46ce05126dea0b01d591f5bef04
SamruddhaChavan007 <samruddhachavan111@gmail.com>
2025-04-20 20:47:18 -0700

    Initial commit

---
+++ app/src/main/java/com/example/synapse/MainAppComposable.kt
@@ -0,0 +1,32 @@
+package com.example.synapse
+
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.synapse.feature.auth.signin.SignInScreen
+import com.example.synapse.feature.auth.signup.SignUpScreen
+
+@Composable
+
+fun MainApp(){
+    Surface(modifier = Modifier.fillMaxSize()) {
+        val navController = rememberNavController()
+
+
+
+        NavHost(navController = navController, startDestination = "login"){
+
+            composable("login") {
+                SignInScreen(navController)
+            }
+            composable("signup") {
+                SignUpScreen(navController)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/synapse/feature/auth/signup/SignUpScreen.kt
@@ -0,0 +1,103 @@
+package com.example.synapse.feature.auth.signup
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.synapse.R
+import java.util.logging.Logger
+
+@Composable
+fun SignUpScreen(navController: NavController) {
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    var username by remember {
+        mutableStateOf("")
+    }
+    Scaffold(modifier = Modifier.fillMaxSize()) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(it)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo), contentDescription = null,
+                modifier = Modifier
+                    .size(200.dp)
+                    .background(Color.White)
+            )
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                modifier = Modifier.fillMaxWidth(),
+                //placeholder = { Text(text = "Username")},
+                label = { Text(text = "Username")} )
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                modifier = Modifier.fillMaxWidth(),
+                //placeholder = { Text(text = "Email")},
+                label = { Text(text = "Email")} )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = PasswordVisualTransformation(),
+                //placeholder = {Text(text = "Password")},
+                label = { Text(text = "Password") })
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = PasswordVisualTransformation(),
+                //placeholder = {Text(text = " Confirm Password")},
+                label = { Text(text = " Confirm Password") })
+            Spacer(modifier = Modifier.size(16.dp))
+            Button(onClick = {/*ToDo*/ }, modifier = Modifier.fillMaxWidth()) {
+                Text(text = "Sign In")
+            }
+            TextButton(onClick = { /*ToDo*/ }) {
+                Text(text = "Don't have an account? Sign Up")
+                Logger.getAnonymousLogger()
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSignUpScreen() {
+    SignUpScreen(navController = rememberNavController())
+}
