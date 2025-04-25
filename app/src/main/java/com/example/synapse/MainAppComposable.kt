d32869a25280bc0d7da7d3b53037d1718eaa734f
SamruddhaChavan007 <samruddhachavan111@gmail.com>
2025-04-24 17:35:34 -0700

    Implemented chat functionality between users.

--- app/src/main/java/com/example/synapse/MainAppComposable.kt
+++ app/src/main/java/com/example/synapse/MainAppComposable.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.synapse.feature.auth.signin.SignInScreen
 import com.example.synapse.feature.auth.signup.SignUpScreen
+import com.example.synapse.feature.chat.ChatScreen
 import com.example.synapse.feature.home.HomeScreen
 import com.google.firebase.auth.FirebaseAuth
 
@@ -34,6 +37,14 @@ fun MainApp() {
             }
             composable("home") {
                 HomeScreen(navController)
+            }
+            composable("chat/{channelId}", arguments = listOf(
+                navArgument("channelId") {
+                    type = NavType.StringType
+                }
+            )) {
+                val channelId = it.arguments?.getString("channelId") ?: ""
+                ChatScreen(navController, channelId)
             }
         }
     }
